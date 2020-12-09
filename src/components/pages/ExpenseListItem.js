75dc830d57757fc32e67bbe7a6f87a965fa81b88
Mary Kelly <mk-km@live.com>
2020-12-09 02:05:31 -0500

    set up fomratting for expense items amount and created time

--- src/components/pages/ExpenseListItem.js
+++ src/components/pages/ExpenseListItem.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({ description, amount, createdAt, id }) => {
     return ( 
         <div>
             <Link to={`/edit/${id}`}><h4>{description}</h4></Link>
-            <p>{amount} - {createdAt}</p>
+            <p>{numeral(amount / 100).format('$0,0.00')} 
+            - 
+            {moment(createdAt).format('MMMM Do, YYYY')}</p>
         </div>
     )}
 
